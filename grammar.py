426570442fc17bf40ad8c940521bce16dfacdcf5
Lee <leeee4990@gmail.com>
2025-09-27 19:03:27 -0400

    edited new_alphabet thing

--- grammar.py
+++ grammar.py
@@ -2,7 +2,7 @@
 # this is the grammatical structure of Artificial
 # adjectives change based on gender and then number of nouns / pronouns
 # order of adjectives adhere to English rules
-# case system
+# uses Russian case system (to an extent)
 
 def single_words():
     while 
